51f515d4653d2ccf25df6e798b63b7c6faeae3da
Grippr <joelkramer713@gmail.com>
2024-10-27 11:56:37 -0400

    Experiment with gcal

--- MagInkCalPy/gcal/gcal.py
+++ MagInkCalPy/gcal/gcal.py
@@ -18,6 +18,7 @@ import logging
 import requests
 from icalendar import Calendar
 from MagInkCalPy.config.config_info import ConfigInfo
+from dateutil.rrule import rrulestr
 
 # -----------------------------------------------------------------------------
 # Utilities
@@ -27,7 +28,9 @@ def _adjust_end_time(endTime):
     # check if end time is at 00:00 of next day, if so set to max time for day before
     if isinstance(endTime, dt.datetime):
         if endTime.time() == dt.time.min:
+            tz = endTime.tzinfo
             endTime = dt.datetime.combine(endTime.date() - dt.timedelta(days=1), dt.time.max)
+            endTime = endTime.replace(tzinfo=tz)
     else: 
         # It's a dateime.date object, meaning it's an all-day event. 
         # In ICS, the end date is exclusive, meaning the end day is actually the
@@ -53,6 +56,11 @@ class GcalHelper:
         # Get the calendar
         self.logger.info("Requesting events")
         response = requests.get(ical_url)
+
+        if startDatetime.tzinfo is None:
+            startDatetime = startDatetime.replace(tzinfo=localTZ)
+        if endDatetime.tzinfo is None:
+            endDatetime = endDatetime.replace(tzinfo=localTZ)
 
         # Parse the calendar
         self.logger.info("Parsing events")
@@ -81,7 +89,28 @@ class GcalHelper:
                 event['end'] = end
                 event['isMultiday'] = _is_multiday(start, end)
                 event['allday'] = allday
-                events.append(event)
+
+                rrule = component.get('rrule')
+                if rrule:
+                    print(event["isMultiday"], event["allday"], event["summary"])
+                    rule = rrulestr(rrule.to_ical().decode('utf-8'), dtstart=start)
+                    
+
+                    for occurrence in rule.between(s_dt, e_dt, inc=True):
+                        recurring_event = event.copy()
+                        recurring_event['start'] = occurrence
+                        recurring_event['end'] = occurrence + (end - start)
+                        events.append(recurring_event)
+                else:
+                    # Filter events based on date range
+                    if isinstance(start, dt.datetime) and isinstance(end, dt.datetime):
+                        if (startDatetime <= start <= endDatetime) or (startDatetime <= end <= endDatetime):
+                            events.append(event)
+                    elif isinstance(start, dt.date) and isinstance(end, dt.date):
+                        if (startDatetime.date() <= start <= endDatetime.date()) or (startDatetime.date() <= end <= endDatetime.date()):
+                            events.append(event)
+                    else:
+                        self.logger.error("Event start and end times are not of the same type")
             else:
                 print(component.name)
                 for key in component:
